01fc1f907fe7a42a5ca79d8529efe1adaecd6769
MD TABISH FARHAN <150028841+tabishfarhan7@users.noreply.github.com>
2025-03-08 13:17:23 +0530

    updated script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const quotes = [];
 let currentQuote = {};
 let audio = null;
 let isMuted = true;
-let appURL = "https://mgks.github.io/zen";
+let appURL = "https://quranic-wisdom.vercel.app/";
 
 // load quotes from JSON
 async function loadQuotes() {
